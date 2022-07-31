43c1ff2bc024caa5bba3b80d61ffacd2fb77eeca
Matt <carlotta.matt@gmail.com>
2022-07-31 15:01:22 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,93 @@
+const chalk = require("chalk");
+const cors = require("cors");
+const express = require("express");
+const morgan = require("morgan");
+const process = require("process");
+const { resolve } = require("path");
+
+const { NODE_ENV } = process.env;
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const app = express();
+const router = express.Router();
+const currentDirectory = process.cwd();
+const { API_PORT, CLIENT, PORT, REACT_APP_API } = process.env;
+const inProduction = NODE_ENV === "production";
+
+// allow incoming requests to come from client only
+app.use(
+  cors({
+    origin: CLIENT,
+  })
+);
+
+// request logging framework
+app.use(morgan("tiny"));
+
+// *optional* parses incoming JSON args to the "req.body"
+// https://expressjs.com/en/api.html#express.json
+app.use(express.json());
+
+// tell express to use router instance
+app.use(router);
+
+// set up a route to listen for "GET" requests to "/api/orders"
+// https://expressjs.com/en/api.html#router.METHOD
+router.get("/api/orders", async (_req, res) => {
+  try {
+    // const res = await WooCommerce.get("orders");
+    const orders = [
+      {
+        id: 1,
+        orderId: 123,
+        customer: "John Doe",
+        email: "johndoe@example.com",
+        items: [{ id: 69420, name: "Banana", quantity: 3 }],
+      },
+    ];
+
+    // assuming res.data is an array of orders
+    // if(!res.data || !res.data.length) throw Error("No orders were found!");
+
+    // sets status: https://expressjs.com/en/api.html#res.status
+    // sends JSON: https://expressjs.com/en/api.html#res.json
+    return res.status(200).json({ orders });
+  } catch (error) {
+    // in this case, sends a plain text response
+    // https://expressjs.com/en/api.html#res.send
+    return res.status(404).send(error.toString());
+  }
+});
+
+// express serve up production assets from build
+if (inProduction) {
+  app.use(express.static("build"));
+
+  // express will serve up the front-end index.html file if it doesn't recognize the route
+  app.get("*", (_req, res) =>
+    res.sendFile(resolve(`${currentDirectory}/build/index.html`))
+  );
+}
+
+// create server on specified PORT
+app.listen(API_PORT || PORT, (err) => {
+  if (!err) {
+    if (inProduction)
+      console.log(
+        chalk.blue(
+          `\nListening for app requests to ${chalk.bold.yellow(CLIENT)}`
+        )
+      );
+
+    console.log(
+      chalk.blue(
+        `Listening for API requests to ${chalk.bold.yellow(REACT_APP_API)}\n`
+      )
+    );
+  } else {
+    console.err(chalk.red(`\nUnable to start server: ${err}`));
+  }
+});
--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [orders, setOrders] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const res = await fetch(`${process.env.REACT_APP_API}/orders`);
+        const data = await res.json();
+
+        setOrders(data.orders);
+      } catch (error) {
+        setError(error.toString());
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchOrders();
+  }, []);
+
+  if (error) return <h1 className="error">Error: {error}</h1>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        {!isLoading ? (
+          <>
+            <h1>Orders</h1>
+            <pre className="order-details">
+              <code>{JSON.stringify(orders, null, 2)}</code>
+            </pre>
+          </>
+        ) : (
+          <h1 className="loading-orders">Loading...</h1>
+        )}
       </header>
     </div>
   );
